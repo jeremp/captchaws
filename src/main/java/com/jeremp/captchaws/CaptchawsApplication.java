b2ff2a8c46928d7d19684d759392d0c31466df0a
Jeremy Passeron <jpasseron@gmail.com>
2017-06-09 20:32:17 +0200

    let's go

---
+++ src/main/java/com/jeremp/captchaws/CaptchawsApplication.java
@@ -0,0 +1,12 @@
+package com.jeremp.captchaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptchawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptchawsApplication.class, args);
+	}
+}
